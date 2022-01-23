72b24bac0f83f26f55698fedd05a0b8b528be588
Hanna-Kaarina <hanna.and@myy.haaga-helia.fi>
2022-01-23 20:14:18 +0200

    all done

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [input, setInput] = useState('');
+  const [score, setScore] = useState('0');
+  const [message, setMessage] = useState('Guess a number between 1 - 100');
+
+  const buttonPressed= () => {
+
+    if (input < Math.floor(Math.random() * 100) + 1) {
+        setMessage('Your guess ' + input + ' is too low.');
+        setScore(parseInt(score) + 1);
+    } else if (input > Math.floor(Math.random() * 100) + 1) {
+        setMessage('Your guess ' + input + ' is too high.');
+        setScore(parseInt(score) + 1);
+    } else {
+       Alert.alert('You guessed the number in ' + score + ' guesses');
+  }
+};
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> {message} </Text>
+      <TextInput
+      style={styles.input} 
+      keyboardType= 'numeric'
+      onChangeText={(input) => setInput(input)}
+      value={input}
+    />
+    <Button onPress= {buttonPressed} title='MAKE GUESS' />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: '30%',
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'white'
+  }
+
 });
